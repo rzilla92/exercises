17dbe87ca8a9ffb14fe3c63329e1cc97b055f955
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-08-29 13:11:51 +0700

    Update Question1.js
    added thought process

--- JS logic/Question1.js
+++ JS logic/Question1.js
@@ -5,6 +5,20 @@ For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20
 Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
 */
 
+/*
+    my thought process
+    to solve the first input.
+        1. Needs to be sorted
+        2. group same numbers into an array
+        3. return in a new array with the sub-arrays in sorted manner
+
+    to solve input 2 and 3.
+        1. needs to be sorted
+        2. filter number and strings into temp arrays
+        3. group same numbers into an array
+        4. return in a new array with the temp string array and temp number array (including sub-arrays) in sorted manner
+*/
+
 const input1 = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20]
 //bonus inputs
 const input2 = [1, "2", "3", 2]
@@ -19,6 +33,7 @@ function arraySort(arr) {
     const numArray = arr.filter((value)=> {return typeof value==="number"})
 
     // .reduce to combine same number into an array, then merge back into the array
+    // r = previousValue; v = currentValue; i = currentIndex ; a = array
     const groupNumArray= numArray.reduce((r, v, i, a) => {
         if (v === a[i - 1]) {
             r[r.length - 1].push(v);
